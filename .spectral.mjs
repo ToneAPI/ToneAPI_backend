a0d58dcad9325ed2b0d5620566382131ddf4283c
nathan.tienyou@viacesi.fr <legonzaur@live.fr>
2023-03-01 12:01:34 +0100

    Starting working on OpenAPI

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NzgwOA/spectral.js?branch_slug=main&token=1c4d01cb-5ad4-463f-be84-11b533c2775d";
+export default { extends: ruleset };
