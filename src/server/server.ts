9d235139ab1d9d04fff7ad234fe07af0ec322ef4
legonzaur <legonzaur@live.fr>
2023-03-22 17:00:42 +0100

    remove authentification try console log

--- src/server/server.ts
+++ src/server/server.ts
@@ -22,7 +22,6 @@ router.post(
   //Huge mess to retrieve server id from expressBasicAuth. We probably should fix it.
   (req, res, next) => {
     if (!req) res.sendStatus(500)
-    console.log('authentification try with', req.headers['authorization'])
     return expressBasicAuth({
       authorizeAsync: true,
       authorizer: CheckServerToken.bind(req),
