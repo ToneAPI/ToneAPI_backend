78ee045fee557eb39f4f1f16e0c2ac3c80d3ce28
legonzaur <legonzaur@live.fr>
2023-03-22 16:59:40 +0100

    add body console log on postges error

--- src/server/server.ts
+++ src/server/server.ts
@@ -199,6 +199,41 @@ router.post(
       })
       .catch((e) => {
         res.sendStatus(500)
+        console.log({
+          killstat_version,
+          server: Number(req.params.serverId),
+          match_id,
+          game_mode,
+          map,
+          game_time,
+          player_count,
+          attacker_name,
+          attacker_id,
+          attacker_current_weapon,
+          attacker_current_weapon_mods,
+          attacker_weapon_1,
+          attacker_weapon_1_mods,
+          attacker_weapon_2,
+          attacker_weapon_2_mods,
+          attacker_weapon_3,
+          attacker_weapon_3_mods,
+          attacker_offhand_weapon_1,
+          attacker_offhand_weapon_2,
+          victim_name,
+          victim_id,
+          victim_current_weapon,
+          victim_current_weapon_mods,
+          victim_weapon_1,
+          victim_weapon_1_mods,
+          victim_weapon_2,
+          victim_weapon_2_mods,
+          victim_weapon_3,
+          victim_weapon_3_mods,
+          victim_offhand_weapon_1,
+          victim_offhand_weapon_2,
+          cause_of_death,
+          distance
+        })
         console.error(e)
       })
   }
